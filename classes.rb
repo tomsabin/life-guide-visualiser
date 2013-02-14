ee728e9e716e169bb647acc9212fa2e5c7d7fa5a
Tom Sabin <tom.sabin@mac.com>
2013-02-14 15:09:02 +0000

    Removed Vertex class, using 'connected_nodes' variable instead. Started on trying to parse the logic file

--- classes.rb
+++ classes.rb
@@ -4,32 +4,17 @@ class Node
   def initialize(name, group)
     @name = name
     @group = group
+    @connected_vetices = []
+  end
+  
+  def add_node(node_id)
+    @connected_vetices.push(node_id)
   end
   
   def to_json(*a)
     {
       'name' => @name,
       'group' => @group
-    }.to_json(*a)
-  end
-
-  def self.json_create(o)
-    new(*o['data'])
-  end
-end
-
-class Vertex
-  def initialize(source, target, value)
-    @source = source
-    @target = target
-    @value = value
-  end
-  
-  def to_json(*a)
-    {
-      'source' => @source,
-      'target' => @target,
-      'value' => @value
     }.to_json(*a)
   end
 
--- web.rb
+++ web.rb
@@ -13,11 +13,27 @@ post '/dataUpload/?' do
   nodes, links = [], []
   raw = request.env["rack.input"].read
   raw = raw.split(/\r\n/)
-  raw.each do |line|
-    if line =~ /show\s[^\.]*$/
-      nodes.push(Node.new(line.split(' ')[1], 0))
+  # raw.each do |line|
+  #   if line =~ /show\s[^\.]*$/
+  #     nodes.push(Node.new(line.split(' ')[1], 0))
+  #   end
+  # end
+  
+  for i in 0...raw.length
+    if raw[i] =~ /show\s[^\.]*$/
+      current_node = Node.new(raw[i].split(' ')[1], 0)
+      nodes.push(current_node)
+      if raw[i+1] =~ /after\s.*\sif\s\(.*\)\sgoto\s(.*)/
+        #after\s.*\sif\s((isempty|and|or)\s)?\(.*\)\sgoto\s(.*)
+        current_node.add_node(raw[i+1].split(' ').last)
+        if raw[i+2] =~ /show\s[^\.]*$/
+          current_node.add_node(raw[i+2].split(' ').last)
+        end
+      end
     end
   end
+  
+  nodes.each { |x| puts x.inspect }
 
   { 
     :nodes => nodes,
