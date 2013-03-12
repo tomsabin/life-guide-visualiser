936268fb7f80b0cebf3f2e97be9e15baa2207dc1
Tom Sabin <tom.sabin@mac.com>
2013-03-12 11:31:36 +0000

    Altered section parsing logic for 'begin section? name'. Seems to have introduced a weird colouring bug

--- methods.rb
+++ methods.rb
@@ -3,9 +3,11 @@
 def clean_lgil(raw)
   # raw.map! { |line| line == "" ? " " : line }
   raw.delete_if { |line| line == '' }
-  raw.delete_if { |line| line =~ /(^(?:(?!(show\s[^\.]+(if\s.+)?$)|after|begin|end).)+$|^((show\s[^\.]+(if\s.+)?$)|after|begin|end)?\t*\s*#+.*)/ }
+  # raw.delete_if { |line| line =~ /(^(?:(?!(show\s[^\.]+(if\s.+)?$)|after|begin|end).)+$|^((show\s[^\.]+(if\s.+)?$)|after|begin|end)?\t*\s*#+.*)/ }
+  raw.keep_if { |line| line =~ /^(show\s[^\.]+(if\s.+)?$|after.+$|begin.+$|end.+$)/ }
   raw.map! { |line| line =~ /^(show|after).+(\s|\t)*#.*/ ? line.slice(0...(line.index('#'))).rstrip : line }
   @raw = raw
+  # puts @raw
 end
 
 def create_nodes(nodes, links)
@@ -20,7 +22,7 @@ end
 def find_sections(nodes)
   @sections = []
   group_int = 3
-  @raw.count { |line| line =~ /begin\ssection.*/ }.times do
+  @raw.count { |line| line =~ /begin(\ssection)?.*/ }.times do
     find_section(@sections)
   end
   @sections.each do |section|
@@ -34,16 +36,19 @@ def find_sections(nodes)
     group_int += 1
   end
   @raw.delete_if { |line| line == 'deleteme' }
+  # puts @sections
+  # puts @raw[680..720]
 end
 
 def find_section(arr)
+  section_name = @raw.find { |line| line =~ /begin(\ssection)?.*/ }
   arr.push({
-    :section_name => @raw.find { |line| line =~ /begin\ssection.*/ }.split(' ')[2],
-    :begin_index => @raw.find_index { |line| line =~ /begin\ssection.*/ } + 1,
-    :end_index => @raw.find_index { |line| line =~ /end\ssection.*/ } - 1
+    :section_name => section_name =~ /begin\ssection.*/ ? section_name.split(' ')[2] : section_name.split(' ')[1],
+    :begin_index => @raw.find_index { |line| line =~ /begin(\ssection)?.*/ } + 1,
+    :end_index => @raw.find_index { |line| line =~ /end(\ssection)?.*/ } - 1
   })
-  @raw[@raw.find_index { |line| line =~ /begin\ssection.*/ }] = 'deleteme'
-  @raw[@raw.find_index { |line| line =~ /end\ssection.*/ }] = 'deleteme'
+  @raw[@raw.find_index { |line| line =~ /begin(\ssection)?.*/ }] = 'deleteme'
+  @raw[@raw.find_index { |line| line =~ /end(\ssection)?.*/ }] = 'deleteme'
 end
 
 def parse_lgil
